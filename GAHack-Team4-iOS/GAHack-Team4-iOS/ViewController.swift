fbbf67b1ee26a6de29e1b65d9bc20cb8975aa0a5
Loris Mazloum <loris.mazloum@gmail.com>
2016-10-28 11:17:01 -0700

    initial project commit

---
+++ GAHack-Team4-iOS/GAHack-Team4-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GAHack-Team4-iOS
+//
+//  Created by Loris Mazloum on 10/28/16.
+//  Copyright © 2016 Maz Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
